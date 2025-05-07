4aa21c709a4ea360d67a3ecd6aff2b8c6fb50889
juandhm18 <juandhm20@gmail.com>
2025-05-07 13:35:09 -0500

    actualizacion codigo

--- S3_Ejercicios/M1S3_Entrenamiento.py
+++ S3_Ejercicios/M1S3_Entrenamiento.py
@@ -1,14 +1,14 @@
-productos = {"papa":{"precio":2500,"cantidad":50}}
+productos = {"papa":{"precio":2500,"cantidad":50},
+             "zanahoria":{"precio":500,"cantidad":10}}
 
 def agregar(articulo, precio, cantidad):
     productos[articulo] = {"precio": precio, "cantidad":cantidad}
     
 def buscar(consul):
-    if consul in productos:
-        for articulo, datos in productos.items():
-            print(f"{articulo}: ${datos['precio']} | cantidad {datos['cantidad']}")
-    else:
-        print("Este articulo no existe")
+        if consul in productos:
+            print(f"\n{consul}: ${productos[consul]['precio']} | cantidad {productos[consul]['cantidad']}")
+        else:
+            print("Este articulo no existe")
 
 def editar(consul, precio):
     if consul in productos:
@@ -24,11 +24,10 @@ def eliminar(consul):
         print("El producto no existe, por lo cual no se puede eliminar")
         
 def total():
-    for nombre, datos in productos.items():
-        print(f"{nombre}: Edad {datos['edad']} | Nota {datos['nota']}")
+    totalInventario = sum(map(lambda x: x['precio']*x['cantidad'], productos.values()))
+    print(totalInventario)
         
 while True:
-    try:
         print("\n     -----DIRECTORIO-----\n")
         print("1. Agregar un nuevo producto")
         print("2. Consultar productos")
@@ -36,29 +35,27 @@ while True:
         print("4. Eliminar productos")
         print("5. Total productos")
         print("6. Cerrar\n")
-        opcion = int(input("Ingresa la opcion: "))
+        opcion = input("Ingresa la opcion: ")
         match opcion:
-            case 1:
+            case "1":
                 articulo = input("Ingrese el nombre del articulo: ")
                 precio = float(input("Ingrese el valor PU de el articulo: "))
                 cantidad = int(input("Cantidad disponible "))
                 agregar(articulo, precio, cantidad)
-            case 2:
-                consul = input("Ingrese el nombre del contacto: ").strip().lower()
+            case "2":
+                consul = input("Ingrese el nombre del producto: ").strip().lower()
                 buscar(consul)
-            case 3:
+            case "3":
                 consul = input("Ingrese el articulo a editar: ")
                 precio = float(input("Ingrese el nuevo precio: "))
                 editar(consul, precio)
-            case 4:
+            case "4":
                 consul = input("Ingrese el articulo a eliminar: ")
                 eliminar(consul)
-            case 5:
+            case "5":
                 total()
-            case 6:
+            case "6":
                 print("Saliendo...")
                 break
             case _:
                 print("Esta opcion no existe, intentalo de nuevo\n")            
-    except ValueError:
-        print("Debes ingresar un número válido.")
