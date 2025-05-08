49942bb04443c2f6d4ff5e87bac7e3f8d2150731
juandhm18 <juandhm20@gmail.com>
2025-05-08 13:06:32 -0500

    validacion añadida

--- S3_Ejercicios/M1S3_Entrenamiento.py
+++ S3_Ejercicios/M1S3_Entrenamiento.py
@@ -4,6 +4,35 @@ productos = {"papa":{"precio":2500,"cantidad":50},
 def agregar(articulo, precio, cantidad):
     productos[articulo] = {"precio": precio, "cantidad":cantidad}
     
+def solicitar_precio():
+    #Solicita y valida que el precio sea un número flotante válido.
+    while True:
+        try:
+            precio = float(input("Ingrese el valor unitario del artículo: "))
+            if precio <= 0:
+                print("El precio debe ser mayor que 0. Intente nuevamente.")
+            else:
+                return precio
+        except ValueError:
+            print("Entrada inválida. Por favor, ingrese un número válido para el precio.")
+
+# Función para solicitar un número entero válido
+def solicitar_cantidad():
+    #Solicita y valida que la cantidad sea un número entero válido.
+    while True:
+        
+        cantidad = input("Ingrese la cantidad disponible: ")
+        if cantidad.replace(" ","").replace(".","",1).isdigit():
+            cantidad = float(cantidad)
+            if cantidad < 0:
+                print("La cantidad no puede ser negativa. Intente nuevamente.")
+            else:
+                return cantidad
+        else:
+            print("Entrada inválida. Por favor, ingrese un número válido para la cantidad.\n")
+
+# Menú interactivo principal
+    
 def buscar(consul):
         if consul in productos:
             print(f"\n{consul}: ${productos[consul]['precio']} | cantidad {productos[consul]['cantidad']}")
@@ -28,6 +57,7 @@ def total():
     print(totalInventario)
         
 while True:
+        #Menu interactivo principal
         print("\n     -----DIRECTORIO-----\n")
         print("1. Agregar un nuevo producto")
         print("2. Consultar productos")
@@ -39,8 +69,8 @@ while True:
         match opcion:
             case "1":
                 articulo = input("Ingrese el nombre del articulo: ")
-                precio = float(input("Ingrese el valor PU de el articulo: "))
-                cantidad = int(input("Cantidad disponible "))
+                precio = solicitar_precio()
+                cantidad = solicitar_cantidad()
                 agregar(articulo, precio, cantidad)
             case "2":
                 consul = input("Ingrese el nombre del producto: ").strip().lower()
@@ -59,3 +89,9 @@ while True:
                 break
             case _:
                 print("Esta opcion no existe, intentalo de nuevo\n")            
+
+# for i in range(10):
+#     variable = input("Go diego go: ")
+#     tupla = (variable)
+#     print(tupla)
+    
