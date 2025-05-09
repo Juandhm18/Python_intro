7e855f964cd0a50fd659355bf5564e9ac6faf12e
juandhm18 <juandhm20@gmail.com>
2025-05-09 09:54:22 -0500

    Actualizacion archivo y nuevo proyecto

---
+++ S3_Ejercicios/04.GestionLibreria.py
@@ -0,0 +1,117 @@
+inventory = [
+    {"title": "book 1", "price": 10.0, "quantity": 100},
+    {"title": "book 2", "price": 15.0, "quantity": 50},
+    {"title": "book 3", "price": 20.0, "quantity": 30},
+    {"title": "book 4", "price": 25.0, "quantity": 10},
+    {"title": "book 5", "price": 30.0, "quantity": 5}
+]
+
+def existing_book():
+    while True:
+        book = input("Please indicate the name of the book: ").lower()
+        found = False
+        for i in inventory:
+            if i["title"] == book:
+                found = True
+                print(f"the {book} already exists so we are unable to add it, however you can edit the quantity")
+                return found
+        if not found:
+            return book
+        
+def request_price():
+    while True:
+        try: 
+            price = float(input("Please type the price of the book: "))
+            if price > 0:
+                return price
+            else:
+                print("The price must be a positive number higher than zero, please try again\n")
+        except ValueError:
+            print("--\033[31mERROR\033[0m-- The value is incorrect, please type a valid number.\n")
+            
+def request_quantity():
+    while True:
+        try: 
+            quantity = int(input("Please type the quantity of the book: "))
+            if quantity >= 0:
+                return quantity
+            else:
+                print("The quantity must be a positive number higher than zero, please try again\n")
+        except ValueError:
+            print("--\033[31mERROR\033[0m-- The value is incorrect, please type a valid number.\n")
+            
+def add_book(new_book, price, quantity):
+    inventory.append({"title":new_book,"price":price,"quantity":quantity})
+    
+def find_book(book):
+    found = False
+    for i in inventory:
+        if i["title"] == book:
+            found = True
+            print(f"the {book} was found an here is the information\n{i["title"]} | {i["price"]} | {i["quantity"]}")
+    if not found:
+        print(f"The book you're trying to find doesn't exist at the moment\n")
+
+def update_price(book, price):
+    found = False    
+    for i in inventory:
+        if i["title"] == book:
+            found = True
+            i["price"] = price
+            print(f"the {book} was found an here is the information\n{i["title"]} | {i["price"]} | {i["quantity"]}")
+    if not found:
+        print(f"The book you're trying to edit doesn't exist\n")    
+
+def delete_book(book):
+    found = False    
+    for i in inventory:
+        if i["title"] == book:
+            found = True
+            inventory.remove(i)
+            return
+    if not found:
+        print(f"The book you're trying to delete doesn't exist in the invetory\n")        
+
+def total_amount():
+    sum = 0
+    for i in inventory:
+        print(f"{i["title"]} | {i["price"]} | {i["quantity"]}")
+        product = i["price"]*i["quantity"]
+        sum = sum + product
+    print(f"\n\033[32mTOTAL\033[0m:  {sum:.2f}")
+    
+while True:
+        #Menu interactivo principal
+        print("""
+            -----\033[33mLIBRERY\033[0m-----\n
+        1. Add new book to the inventary
+        2. Consult book
+        3. Update the price of the book
+        4. Delete book
+        5. Total inventory
+        6. Close application\n""")
+        option = input("Choose an option: ")
+        match option:
+            case "1":
+                new_book = existing_book()
+                if new_book != True:
+                    price = request_price()
+                    quantity = request_quantity()
+                    add_book(new_book, price, quantity)
+            case "2":
+                book = input("Type the name of the book you are looking for: ").lower()
+                find_book(book)
+            case "3":
+                book = input("Type the name of the book you want to update: ").lower()
+                price = request_price()
+                update_price(book, price)
+            case "4":
+                book = input("Type the book you want to delete: ").lower()
+                delete_book(book)
+            case "5":
+                total_amount()
+            case "6":
+                print("\033[35mClosing...\033[0m")
+                break
+            case _:
+                print("The option is not eligible or doesn't exist, please try again\n")           
--- S3_Ejercicios/M1S3_Entrenamiento.py
+++ S3_Ejercicios/M1S3_Entrenamiento.py
@@ -1,64 +1,93 @@
-productos = {"papa":{"precio":2500,"cantidad":50},
-             "zanahoria":{"precio":500,"cantidad":10}}
-
-def agregar(articulo, precio, cantidad):
-    productos[articulo] = {"precio": precio, "cantidad":cantidad}
-    
-def solicitar_precio():
-    #Solicita y valida que el precio sea un número flotante válido.
+# Lista de productos con información básica: nombre, precio y cantidad
+productos = [{"producto":"papa","precio":25,"cantidad":50},
+             {"producto":"zanahoria","precio":50,"cantidad":10}]
+# Función que verifica si un artículo ya existe en la lista de productos
+def articulo_existente():
     while True:
-        try:
-            precio = float(input("Ingrese el valor unitario del artículo: "))
-            if precio <= 0:
-                print("El precio debe ser mayor que 0. Intente nuevamente.")
+        articulo = input("Ingrese el nombre del articulo: ").lower()
+        found = False
+        for i in productos:
+            #Si el articulo existe se retorna true al codigo
+            if i["producto"] == articulo:
+                found = True
+                print("""El articulo ya existe por lo cual no se puede agregar de nuevo,
+pero se puede editar su cantidad""")
+                return found
+        #SI el articulo no existe retorna el nombre del articulo ingresado
+        if not found:
+            return articulo 
+# Función para agregar un nuevo producto al inventario
+def agregar(articulo, precio, cantidad):
+    productos.append({"producto":articulo,"precio":precio,"cantidad":cantidad})
+#Solicita y valida que el precio sea un número flotante válido.
+def solicitar_precio():
+    while True:
+            precio = input("Ingrese el valor unitario del artículo: ")
+            if precio.isdigit(): # Verifica que el input sea numérico
+                precio = float(precio)
+                if precio <= 0:
+                    print("El precio debe ser mayor que 0. Intente nuevamente.")
+                else:
+                    return precio
             else:
-                return precio
-        except ValueError:
-            print("Entrada inválida. Por favor, ingrese un número válido para el precio.")
-
+                print("Entrada inválida. Por favor, ingrese un número válido para el precio.")
 # Función para solicitar un número entero válido
 def solicitar_cantidad():
     #Solicita y valida que la cantidad sea un número entero válido.
-    while True:
-        
+    while True: 
         cantidad = input("Ingrese la cantidad disponible: ")
-        if cantidad.replace(" ","").replace(".","",1).isdigit():
-            cantidad = float(cantidad)
+        if cantidad.isdigit():
+            cantidad = int(cantidad)
             if cantidad < 0:
                 print("La cantidad no puede ser negativa. Intente nuevamente.")
             else:
                 return cantidad
         else:
             print("Entrada inválida. Por favor, ingrese un número válido para la cantidad.\n")
-
-# Menú interactivo principal
-    
+# Función para buscar un producto en el inventario
 def buscar(consul):
-        if consul in productos:
-            print(f"\n{consul}: ${productos[consul]['precio']} | cantidad {productos[consul]['cantidad']}")
-        else:
-            print("Este articulo no existe")
-
+    found = False
+    for i in productos:
+        if i["producto"] == consul:
+            found = True
+            print(f"El articulo fue encontrado y esta es su informacion \n{i["producto"]} | {i["precio"]} | {i["cantidad"]}")
+    # Si el producto no está en la lista, se informa al usuario
+    if not found:
+        print(f"Este articulo no existe\n")
+# Función para actualizar el precio de un producto existente
 def editar(consul, precio):
-    if consul in productos:
-        productos[consul]["precio"] = precio
-        print(f"precio actualizado para {consul}: {productos[consul]['precio']}")
-    else:
-        print("El producto no existe.")
-
+    found = False    
+    for i in productos:
+        if i["producto"] == consul:
+            found = True
+            i["precio"] = precio
+            print(f"El precio de este articulo fue editacon con exito\n")
+    if not found:
+        print(f"El articulo que estas tratando de editar no existe\n") 
+# Función para eliminar un producto del inventario
 def eliminar(consul):
-    if consul in productos:
-        del productos[consul]
-    else:
+    found = False
+    for i in productos:
+        if consul == i["producto"]:
+            found = True
+            productos.remove(i) # Se usa remove() para eliminar el diccionario correcto
+            return
+    if not found:
         print("El producto no existe, por lo cual no se puede eliminar")
-        
+# Función que muestra el inventario y calcula el valor total       
 def total():
-    totalInventario = sum(map(lambda x: x['precio']*x['cantidad'], productos.values()))
-    print(totalInventario)
-        
+    if not productos: 
+        print("El inventario está vacío.")
+        return# Verifica si la lista está vacía
+    for i in productos:
+        print(f"{i["producto"]} | {i["precio"]} | {i["cantidad"]}")
+    # Calcula el valor total del inventario multiplicando precio * cantidad
+    totalInventario = sum(map(lambda x: x['precio']*x['cantidad'], productos))
+    print(f"\n\033[32mTOTAL\033[0m:  {totalInventario:.2f}")
+    
+# Menú interactivo principal
 while True:
-        #Menu interactivo principal
-        print("\n     -----DIRECTORIO-----\n")
+        print("\n     -----\033[33mDIRECTORIO\033[0m-----\n")
         print("1. Agregar un nuevo producto")
         print("2. Consultar productos")
         print("3. Actualizar precios")
@@ -68,10 +97,11 @@ while True:
         opcion = input("Ingresa la opcion: ")
         match opcion:
             case "1":
-                articulo = input("Ingrese el nombre del articulo: ")
-                precio = solicitar_precio()
-                cantidad = solicitar_cantidad()
-                agregar(articulo, precio, cantidad)
+                articulo = articulo_existente()
+                if articulo != True: # Si el artículo no existe, se agregará
+                    cantidad = solicitar_cantidad()
+                    precio = solicitar_precio()
+                    agregar(articulo, precio, cantidad)
             case "2":
                 consul = input("Ingrese el nombre del producto: ").strip().lower()
                 buscar(consul)
@@ -85,13 +115,9 @@ while True:
             case "5":
                 total()
             case "6":
-                print("Saliendo...")
+                print("\033[35mSaliendo...\033[0m")
                 break
             case _:
                 print("Esta opcion no existe, intentalo de nuevo\n")            
 
-# for i in range(10):
-#     variable = input("Go diego go: ")
-#     tupla = (variable)
-#     print(tupla)
     
